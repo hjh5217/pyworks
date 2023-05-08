040ebf24c416e7946c0041ad8cbc8f435b361ee1
hjh5217 <hbh5353@gmail.com>
2023-05-08 10:43:27 +0900

    23/05/08 파이선코드

--- member/static/js/main.js
+++
@@ -1,9 +0,0 @@
-window.onload = function(){
-    setInterval(setWatch, 1000);
-
-      function setWatch() {
-        const date = new Date();
-        let now = date.toLocaleTimeString();
-        document.getElementById("demo").innerHTML = now;
-      }
-}
